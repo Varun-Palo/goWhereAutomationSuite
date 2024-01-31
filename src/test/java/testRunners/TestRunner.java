35c8302004dcea1e312cc2ce0dc1c68195fd4f65
Varun-Palo <157771222+Varun-Palo@users.noreply.github.com>
2024-01-31 15:39:24 +0800

    Update TestRunner.java
    updated correct folder name

--- src/test/java/testRunners/TestRunner.java
+++ src/test/java/testRunners/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = { "src/test/resources/AppFeatures" }, glue = { "AppHooks","StepDefinitions" }, plugin = {
+@CucumberOptions(features = { "src/test/resources/Features" }, glue = { "AppHooks","StepDefinitions" }, plugin = {
 		"json:target/cucumber.json", "pretty" })
 public class TestRunner {
 
